96014b84b46e71fe0d7d7eca70cdec7dc2540a67
sylvia <appoline.MAVOUNGOU-PANGOU@etu.univ-amu.fr>
2026-07-23 01:54:36 +0200

    Découverte du cast

--- Java/Main.java
+++ Java/Main.java
@@ -6,6 +6,13 @@ public class Main {
         int longeur = 180;
         int larg = 120;
         int surface = longeur * larg ;
-        System.out.println(surface);
+        int perimettre = (longeur + larg) * 2;
+        int diff = longeur - larg ;
+        double rapport = (double) larg / longeur; // Ici on a fait un cast pour transformer le résultat de la variable en type float
+        System.out.println("La surface au sol de la voiture est de : "+ surface);
+        System.out.println("Le perimettre au sol de la voiture est de : "+ perimettre);
+        System.out.println("La différence entre la longueur et la largeur est de : "+ diff);
+        System.out.println("Le rapport entre la longueur et la largeur est de : "+ rapport);
+
     }
 }
