ed17cf77daf9dda10cfc57c56745f0eff0a14e63
sylvia <appoline.MAVOUNGOU-PANGOU@etu.univ-amu.fr>
2026-07-23 01:27:48 +0200

    insertion_java

---
+++ Java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args){
+
+        int longeur = 180;
+        int larg = 120;
+        int surface = longeur * larg ;
+        System.out.println(surface);
+    }
+}
